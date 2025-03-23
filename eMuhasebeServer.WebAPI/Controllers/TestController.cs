84d376ec29ad9be7853bb5165fe6442be8681010
EmreCanTERKAN <emrecanterkan06@gmail.com>
2025-03-23 18:30:22 +0300

    smtp4Dev ve FluentEmail.Smtp ile mail gönderme

---
+++ eMuhasebeServer.WebAPI/Controllers/TestController.cs
@@ -0,0 +1,29 @@
+﻿using eMuhasebeServer.WebAPI.Abstractions;
+using FluentEmail.Core;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eMuhasebeServer.WebAPI.Controllers;
+
+[AllowAnonymous]
+public class TestController : ApiController
+{
+    private readonly IFluentEmail _fluentEmail;
+    public TestController(IMediator mediator, IFluentEmail fluentEmail) : base(mediator)
+    {
+        _fluentEmail = fluentEmail;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> SendTestEmail()
+    {
+        await _fluentEmail
+            .To("ect@gmail.com")
+            .Subject("Test maili")
+            .Body("<h1>Mail Gönderme Testi</h1>", true)
+            .SendAsync();
+
+        return NoContent();
+    }
+}
