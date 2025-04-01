4fb7bfff93136db56fca6419750d96c3a7aa7ca7
EmreCanTERKAN <emrecanterkan06@gmail.com>
2025-04-01 17:56:18 +0300

    Bank CRUD işlemleri

---
+++ eMuhasebeServer.Application/Features/Banks/CreateBankCommand.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using eMuhasebeServer.Application.Services;
+using eMuhasebeServer.Domain.Entities;
+using eMuhasebeServer.Domain.Repositories;
+using MediatR;
+using TS.Result;
+
+namespace eMuhasebeServer.Application.Features.Banks;
+public sealed record CreateBankCommand(
+    string Name,
+    string IBAN,
+    int CurrencyTypeValue) : IRequest<Result<string>>;
+
+internal sealed class CreateBankCommandHandler(
+    IBankRepository bankRepository,
+    IUnitOfWorkCompany unitOfWorkCompany,
+    ICacheService cacheService,
+    IMapper mapper) : IRequestHandler<CreateBankCommand, Result<string>>
+{
+    public async Task<Result<string>> Handle(CreateBankCommand request, CancellationToken cancellationToken)
+    {
+        bool isIbanExist = await bankRepository.AnyAsync(p => p.IBAN == request.IBAN, cancellationToken);
+
+        if (isIbanExist)
+        {
+            return Result<string>.Failure("Iban daha önce kadedilmiş");
+        }
+
+        Bank bank = mapper.Map<Bank>(request);
+
+        await bankRepository.AddAsync(bank, cancellationToken);
+        await unitOfWorkCompany.SaveChangesAsync(cancellationToken);
+
+        cacheService.Remove("bank");
+
+        return "Banka başarıyla kaydedildi";
+    }
+}
---
+++ eMuhasebeServer.Application/Features/Banks/UpdateBankCommand.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using eMuhasebeServer.Application.Services;
+using eMuhasebeServer.Domain.Entities;
+using eMuhasebeServer.Domain.Repositories;
+using MediatR;
+using TS.Result;
+
+namespace eMuhasebeServer.Application.Features.Banks;
+public sealed record UpdateBankCommand(
+    Guid Id,
+    string Name,
+    string IBAN,
+    int CurrencyTypeValue) : IRequest<Result<string>>;
+
+internal sealed class UpdateBankCommandHandler(
+    IBankRepository bankRepository,
+    IUnitOfWorkCompany unitOfWorkCompany,
+    ICacheService cacheService,
+    IMapper mapper) : IRequestHandler<UpdateBankCommand, Result<string>>
+{
+    public async Task<Result<string>> Handle(UpdateBankCommand request, CancellationToken cancellationToken)
+    {
+        Bank? bank = await bankRepository.GetByExpressionWithTrackingAsync(p => p.Id == request.Id, cancellationToken);
+
+        if (bank is null)
+        {
+            return Result<string>.Failure("Banka bulunamadı");
+        }
+
+        if(bank.IBAN != request.IBAN)
+        {
+            bool isIbanExist = await bankRepository.AnyAsync(p => p.IBAN == request.IBAN, cancellationToken);
+
+            if (isIbanExist)
+            {
+                return Result<string>.Failure("Iban daha önce kadedilmiş");
+            }
+        }
+
+        mapper.Map(request, bank);
+
+        await unitOfWorkCompany.SaveChangesAsync(cancellationToken);
+
+        cacheService.Remove("banks");
+
+        return "Banka bilgileri başarıyla güncellendi";
+    }
+}
